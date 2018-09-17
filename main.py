fd57cf9cc32939129c5cceb9467e1609aabcc539
Dmitry Bashkatov <dbashkatov@gmail.com>
2018-09-17 22:33:46 +0300

    add command parser

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import shlex
 import logging
 import asyncio
 import argparse
@@ -6,6 +7,7 @@ tcp_buffer_size = 2048
 max_maintenance_command_size = tcp_buffer_size
 log = logging.getLogger(__name__)
 queues = set()
+servers = set()
 
 
 def main():
@@ -18,18 +20,13 @@ def main():
     loop = asyncio.get_event_loop()
     server = loop.run_until_complete(asyncio.start_server(handle_client, '127.0.0.1', 8888))
     maintenance_server = loop.run_until_complete(asyncio.start_server(handle_maintenance_client, '127.0.0.1', 8889))
+    servers.add(server)
+    servers.add(maintenance_server)
 
     print('Serving on {}'.format(server.sockets[0].getsockname()))
     print('Serving maintenance on {}'.format(maintenance_server.sockets[0].getsockname()))
-    try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
 
-    # Close the server
-    server.close()
-    maintenance_server.close()
-    loop.run_until_complete(asyncio.gather(server.wait_closed(), maintenance_server.wait_closed()))
+    loop.run_until_complete(asyncio.gather(*(s.wait_closed() for s in servers)))
     loop.close()
 
 
@@ -47,26 +44,6 @@ async def handle_client(local_reader, local_writer):
         local_writer.close()
 
 
-async def handle_maintenance_client(reader, writer):
-    command = b''
-
-    while not reader.at_eof():
-        if len(command) >= max_maintenance_command_size:
-            log.error('Maintenance command starting with "%s" is too long', command)
-            writer.close()
-            return
-        command += await reader.read(tcp_buffer_size)
-
-    command = command.strip()
-
-    if command == b'qwe':
-        flush('127.0.0.1', 8887)
-    else:
-        log.error('Invalid command: "%s"', command)
-
-    writer.close()
-
-
 async def pipe(reader, writer):
     try:
         while not reader.at_eof():
@@ -75,9 +52,40 @@ async def pipe(reader, writer):
         writer.close()
 
 
-def flush(host, port):
+class MyArgParser(argparse.ArgumentParser):
+    def exit(self, status=0, message=None):
+        raise Exception(message)
+
+
+async def handle_maintenance_client(reader, writer):
+    try:
+        data = b''
+
+        while not reader.at_eof():
+            if len(data) >= max_maintenance_command_size:
+                raise Exception(f'Maintenance command starting with "{data}" is too long')
+            data += await reader.read(tcp_buffer_size)
+
+        argv = shlex.split(data.strip().decode())
+        parser = MyArgParser(prog='')
+        subparsers = parser.add_subparsers(dest='command')
+        flush_parser = subparsers.add_parser('flush')
+        flush_parser.add_argument('host')
+        flush_parser.add_argument('port', type=int)
+
+        args = vars(parser.parse_args(argv))
+        await globals()['handle_command_' + args.pop('command')](**args)
+        writer.write(b'OK\n')
+
+    finally:
+        writer.close()
+
+
+async def handle_command_flush(host, port):
     for queue in queues:
         queue.put_nowait((host, port))
+    for server in servers:
+        server.close()
 
 
 if __name__ == '__main__':
